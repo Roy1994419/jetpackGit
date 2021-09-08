96fe50e00384f3de62c59890507dd0c0c346cacc
Roy <15170322417@163.com>
2021-09-08 17:20:11 +0800

    1.为发布jetpack库简单操作

---
+++ mylibrary/src/main/java/com/roy/mylibrary/Student.kt
@@ -0,0 +1,14 @@
+package com.roy.mylibrary
+
+/**
+ *    desc   :
+ *    e-mail : 1391324949@qq.com
+ *    date   : 2021/9/8 17:19
+ *    author : Roy
+ *    version: 1.0
+ */
+class Student {
+    fun test(){
+
+    }
+}
